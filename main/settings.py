bff7b38a9de9056322b391369b721aa016a8984a
Vladimir-Vologda <vologda2018@inbox.ru>
2022-03-03 11:05:35 +0300

    DATABASE - postgresql

--- main/settings.py
+++ main/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
